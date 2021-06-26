9b5ce034131867b73a8cfc4116c710ce3e33c382
Greenhand007 <348218692@qq.com>
2021-06-26 19:39:56 +0800

    sd

---
+++ chapter2/list2_1.py
@@ -0,0 +1,16 @@
+a,b,c,d,e,f,g,h=range(8)
+
+N=[
+    {b,c,d,e,f},    #a
+    {c,e},          #b
+    {d},            #c
+    {e},            #d
+    {f},            #e
+    {c,g,h},        #f
+    {f,h},          #g
+    {f,g}           #h
+]
+
+print(b in N[a])   #Neighborhood membership
+
+print(len(N[f]))   #Degree

aa7934333cc630ea008cd0d53020a69995f0a91c
Greenhand007 <348218692@qq.com>
2021-06-26 20:30:03 +0800

    ok

--- chapter2/list2_1.py
+++ chapter2/list2_1.py
@@ -1,3 +1,4 @@
+
 a,b,c,d,e,f,g,h=range(8)
 
 N=[
---
+++ chapter2/list2_4.py
@@ -0,0 +1,16 @@
+
+N={
+    'a':set('bcdef'),
+    'b':set('ce'),
+    'c':set('d'),
+    'd':set('e'),
+    'e':set('f'),
+    'f':set('cgh'),
+    'g':set('fh'),
+    'h':set('fg')
+}
+
+print('b' in N)  
+
+
+print(len(N['h'])) 
